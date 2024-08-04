cc8a8472f6cd44be8a406758163d02fbfc3f5d61
lealre <renaanvp@gmail.com>
2024-08-04 18:29:04 +0100

    database: set up database management with SQLAlchemy and Alembic

---
+++ src/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, onupdate=func.now(), nullable=True
+    )

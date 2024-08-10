a01c041760c3a712dffa785bc65222a8711fef66
lealre <renaanvp@gmail.com>
2024-08-10 16:53:10 +0100

    test: Book router with 100% coverage

--- tests/test_book.py
+++ tests/test_book.py
@@ -30,6 +30,16 @@ def test_add_book_already_exists(client, token, book):
     assert response.json() == {'detail': f'{book.title} already in MADR.'}
 
 
+def test_add_book_not_authenticated(client):
+    response = client.post(
+        '/book',
+        json={'year': 2024, 'title': 'book title', 'author_id': 1},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Not authenticated'}
+
+
 def test_delete_book(client, token, book):
     response = client.delete(
         f'/book/{book.id}', headers={'Authorization': f'Bearer {token}'}
@@ -46,6 +56,13 @@ def test_delete_book_not_found(client, token, book):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Book not found in MADR.'}
+
+
+def test_delete_book_not_authenticated(client, book):
+    response = client.delete(f'/book/{book.id}')
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Not authenticated'}
 
 
 def test_patch_book(session, client, token):
@@ -85,6 +102,13 @@ def test_patch_book_not_found(client, token, book):
     assert response.json() == {'detail': 'Book not found in MADR.'}
 
 
+def test_patch_book_not_authenticated(client, book):
+    response = client.patch(f'/book/{10}', json={'year': 2000})
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Not authenticated'}
+
+
 def test_get_book_by_id(client, book):
     response = client.get(f'/book/{book.id}')
 
@@ -102,3 +126,57 @@ def test_get_book_by_id_not_found(client, book):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Book not found in MADR.'}
+
+
+def test_list_books_empty(client):
+    response = client.get('/book')
+
+    assert response.json()['books'] == []
+
+
+def test_list_books_filter_name_should_return_5_books(client, session):
+    expected_books = 5
+    session.bulk_save_objects(BookFactory.create_batch(5))
+    books_with_title = BookFactory.create_batch(5, title='title')
+    for n, book in enumerate(books_with_title):
+        book.title = f'title_{n}'
+    session.bulk_save_objects(books_with_title)
+    session.commit()
+
+    response = client.get('/book/?name=oo')
+
+    assert len(response.json()['books']) == expected_books
+
+
+def test_list_books_filter_year_should_return_5_books(client, session):
+    expected_books = 5
+    session.bulk_save_objects(BookFactory.create_batch(5, year=2000))
+    session.bulk_save_objects(BookFactory.create_batch(5, year=2024))
+    session.commit()
+
+    response = client.get('/book/?year=2000')
+
+    assert len(response.json()['books']) == expected_books
+
+
+def test_list_books_filter_combined_should_return_5_books(session, client):
+    expected_books = 5
+    books = BookFactory.create_batch(7, year=2000)
+    books[-1].title = 'title'
+    books[0].year = 2024
+    session.bulk_save_objects(books)
+    session.commit()
+
+    response = client.get('/book/?year=2000&name=oo')
+
+    assert len(response.json()['books']) == expected_books
+
+
+def test_list_books_pagination_should_return_20_books(session, client):
+    expected_books = 20
+    session.bulk_save_objects(BookFactory.create_batch(25, year=2000))
+    session.commit()
+
+    response = client.get('/book/?title=2000')
+
+    assert len(response.json()['books']) == expected_books

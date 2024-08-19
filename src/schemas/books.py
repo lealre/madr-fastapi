a40234d1ad5408efa149f60453b86aaa47aae448
lealre <renaanvp@gmail.com>
2024-08-19 19:03:48 +0100

    fix schemas: In Book schema, set year to between 1700 and 2024, and ensure Author ID is greater than 0.

--- src/schemas/books.py
+++ src/schemas/books.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field, field_validator
 
 
 class BookSchema(BaseModel):
-    year: int = Field(gt=0)
+    year: int = Field(gt=1700, lt=2025)
     title: str
-    author_id: int
+    author_id: int = Field(gt=0)
 
     @field_validator('title')
     def validate_name(cls, v):
